64d90d45f997e42839caf34257a66492bcf74381
saniaorongxue <277651205@qq.com>
2018-03-09 15:21:58 +0800

    add iview ui

--- backstage/authority management/management/src/components/Main.vue
+++ backstage/authority management/management/src/components/Main.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="Main">
-    <section @click="clickHead">dsafffdsa</section>
+    <section>
+      <Button>Default</Button>
+      <Button type="primary">Primary</Button>
+      <Button type="ghost">Ghost</Button>
+      <Button type="dashed">Dashed</Button>
+      <Button type="text">Text</Button>
+      <Button type="info">Info</Button>
+      <Button type="success">Success</Button>
+      <Button type="warning">Warning</Button>
+      <Button type="error">Error</Button>
+    </section>
   </div>
 </template>
 

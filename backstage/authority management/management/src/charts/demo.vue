78f7e32d91ed39dc4ec2edb531262e1652ae8883
saniaorongxue <277651205@qq.com>
2018-03-22 13:47:29 +0800

    update demo.vue

--- backstage/authority management/management/src/charts/demo.vue
+++ backstage/authority management/management/src/charts/demo.vue
@@ -95,66 +95,129 @@
 
 
         //highcharts demo
-        draw() {
+        /***********************************需要封装********************************
+         ***params***
+         *    @{id}: 绘图的容器
+         *    @{type}: [pie, line, colomun, ...]
+         *    @{content}: 需要显示的内容，根据图形的样式显示内容
+         *    @{options}: 样式图的数据
+         *
+        * *************************************************************************/
+        draw(id, type, content, options) {
+          // this.Highcharts.chart('main', {
+          //   title: {
+          //     text: '2010 ~ 2016 年太阳能行业就业人员发展情况'
+          //   },
+          //   subtitle: {
+          //     text: '数据来源：thesolarfoundation.com'
+          //   },
+          //   yAxis: {
+          //     title: {
+          //       text: '就业人数'
+          //     }
+          //   },
+          //   legend: {
+          //     layout: 'vertical',
+          //     align: 'right',
+          //     verticalAlign: 'middle'
+          //   },
+          //   credits: {
+          //     enabled: false  //去掉hightchats水印
+          //   },
+          //   plotOptions: {
+          //     series: {
+          //       label: {
+          //         connectorAllowed: false
+          //       },
+          //       pointStart: 2010
+          //     }
+          //   },
+          //   series: [{
+          //     name: '安装，实施人员',
+          //     data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+          //   }, {
+          //     name: '工人',
+          //     data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+          //   }, {
+          //     name: '销售',
+          //     data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+          //   }, {
+          //     name: '项目开发',
+          //     data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+          //   }, {
+          //     name: '其他',
+          //     data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+          //   }],
+          //   responsive: {
+          //     rules: [{
+          //       condition: {
+          //         maxWidth: 500
+          //       },
+          //       chartOptions: {
+          //         legend: {
+          //           layout: 'horizontal',
+          //           align: 'center',
+          //           verticalAlign: 'bottom'
+          //         }
+          //       }
+          //     }]
+          //   }
+          // });
           this.Highcharts.chart('main', {
+            chart: {
+              type: 'pie'
+            },
             title: {
-              text: '2010 ~ 2016 年太阳能行业就业人员发展情况'
+              text: '不同国家人口密度及面积对比'
             },
             subtitle: {
-              text: '数据来源：thesolarfoundation.com'
-            },
-            yAxis: {
-              title: {
-                text: '就业人数'
-              }
-            },
-            legend: {
-              layout: 'vertical',
-              align: 'right',
-              verticalAlign: 'middle'
+              text: '扇区长度（圆周方法）表示面积，宽度（纵向）表示人口密度' ,
             },
             credits: {
-              enabled: false  //去掉hightchats水印
-            },
-            plotOptions: {
-              series: {
-                label: {
-                  connectorAllowed: false
+                  enabled: false  //去掉hightchats水印
                 },
-                pointStart: 2010
-              }
+            tooltip: {
+              headerFormat: '',
+              pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
+              '面积 (平方千米): <b>{point.y}</b><br/>' +
+              '人口密度 (每平方千米人数): <b>{point.z}</b><br/>'
             },
             series: [{
-              name: '安装，实施人员',
-              data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-            }, {
-              name: '工人',
-              data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-            }, {
-              name: '销售',
-              data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-            }, {
-              name: '项目开发',
-              data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-            }, {
-              name: '其他',
-              data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-            }],
-            responsive: {
-              rules: [{
-                condition: {
-                  maxWidth: 500
-                },
-                chartOptions: {
-                  legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom'
-                  }
-                }
+              minPointSize: 10,
+              innerSize: '20%',
+              zMin: 0,
+              name: 'countries',
+              data: [{
+                name: '西班牙',
+                y: 505370,
+                z: 92.9
+              }, {
+                name: '法国',
+                y: 551500,
+                z: 118.7
+              }, {
+                name: '波兰',
+                y: 312685,
+                z: 124.6
+              }, {
+                name: '捷克共和国',
+                y: 78867,
+                z: 137.5
+              }, {
+                name: '意大利',
+                y: 301340,
+                z: 201.8
+              }, {
+                name: '瑞士',
+                y: 41277,
+                z: 214.5
+              }, {
+                name: '德国',
+                y: 357022,
+                z: 235.6
               }]
-            }
-          });
+            }]
+          })
         }
       }
     }

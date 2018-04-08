fe8314a13a290948fc2025edd3b44fd0cae00450
saniaorongxue <277651205@qq.com>
2018-04-08 18:29:14 +0800

    update server app.js

--- backstage/authority/management/server/app.js
+++ backstage/authority/management/server/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
-const login = require('./api/user/api'); //调用api
+const user = require('./api/user/api'); //调用api
 const app = express();
 
 //配置session
@@ -17,13 +17,13 @@ app.use(session({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(login);
+app.use(user);
 
 // 访问静态资源文件 这里是访问所有dist目录下的静态资源文件
 app.use(express.static(path.resolve(__dirname, '../dist')))
 // 因为是单页应用 所有请求都走/dist/index.html
 app.get('*', function(req, res) {
-  const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8')
+  const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8');
   res.send(html);
 });
 app.listen(90);

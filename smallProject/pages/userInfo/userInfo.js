bb2adf85f4f4f708aa9f98e49c8a0ba9ce6c51b8
saniaorongxue <277651205@qq.com>
2018-04-09 15:17:34 +0800

    add some dir

---
+++ smallProject/pages/userInfo/userInfo.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+  
+  },
+  //  // 页面初始化 options为页面跳转所带来的参数
+  //  onLoad: function (options) {
+
+  //  },
+  //  // 页面渲染完成
+  //  onReady: function () {
+
+  //  },
+  //  // 页面显示
+  //  onShow: function () {
+
+  //  },
+  //   // 页面隐藏
+  //  onHide: function () {
+
+  //  },
+  //    // 页面关闭
+  //  onUnload: function () {
+
+  //  },
+  
+})

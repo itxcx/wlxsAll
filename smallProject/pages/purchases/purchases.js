a10a7d1c243ea80ff27a9ab15461379ba18ac525
saniaorongxue <277651205@qq.com>
2018-04-10 10:34:46 +0800

    update purchases

--- smallProject/pages/purchases/purchases.js
+++ smallProject/pages/purchases/purchases.js
@@ -1,80 +1,41 @@
 Page({
   data: {
-    winHeight: "",//窗口高度
-    currentTab: 0, //预设当前项的值
-    scrollLeft: 0, //tab标题的滚动条位置
-    expertList: [{ //假数据
-      img: "avatar.png",
-      name: "欢顔",
-      tag: "知名情感博主",
-      answer: 134,
-      listen: 2234
-    }]
-  },
-  // 滚动切换标签样式
-  switchTab: function (e) {
-    this.setData({
-      currentTab: e.detail.current
-    });
-    this.checkCor();
-  },
-  // 点击标题切换当前页时改变样式
-  swichNav: function (e) {
-    var cur = e.target.dataset.current;
-    if (this.data.currentTaB == cur) { return false; }
-    else {
-      this.setData({
-        currentTab: cur
-      })
-    }
-  },
-  //判断当前滚动超过一屏时，设置tab标题滚动条。
-  checkCor: function () {
-    if (this.data.currentTab > 4) {
-      this.setData({
-        scrollLeft: 300
-      })
-    } else {
-      this.setData({
-        scrollLeft: 0
-      })
-    }
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换  
+    currentTab: 0,
   },
   onLoad: function () {
     var that = this;
-    //  高度自适应
     wx.getSystemInfo({
       success: function (res) {
-        var clientHeight = res.windowHeight,
-          clientWidth = res.windowWidth,
-          rpxR = 750 / clientWidth;
-        var calc = clientHeight * rpxR - 180;
-        console.log(calc)
         that.setData({
-          winHeight: calc
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
         });
       }
     });
   },
+  /** 
+     * 滑动切换tab 
+     */
+  bindChange: function (e) {
 
-  //  // 页面初始化 options为页面跳转所带来的参数
-  //  onLoad: function (options) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
 
-  //  },
-  //  // 页面渲染完成
-  //  onReady: function () {
-
-  //  },
-  //  // 页面显示
-  //  onShow: function () {
-
-  //  },
-  //   // 页面隐藏
-  //  onHide: function () {
-
-  //  },
-  //    // 页面关闭
-  //  onUnload: function () {
-
-  //  },
+  },
+  /** 
+   * 点击tab切换 
+   */
+  swichNav: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  }  
 })

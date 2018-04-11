0b7bc177cfeabd110254b740fa7e169b39e271c6
saniaorongxue <277651205@qq.com>
2018-04-11 09:58:26 +0800

    update userInfo

--- smallProject/pages/userInfo/userInfo.js
+++ smallProject/pages/userInfo/userInfo.js
@@ -6,6 +6,10 @@ Page({
   //  // 页面初始化 options为页面跳转所带来的参数
    onLoad: function (options) {
      var userInfo = wx.getStorageSync('userInfo').userInfo;
+     this.setData({
+       nickName: userInfo.nickName,
+       avatarUrl: userInfo.avatarUrl
+     })
    },
   //  // 页面渲染完成
   //  onReady: function () {

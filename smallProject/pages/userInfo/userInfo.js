d20f1e9e2e4f7490d8724ac225961f3ec87b40bb
saniaorongxue <277651205@qq.com>
2018-04-11 11:29:35 +0800

    update userinfo style

--- smallProject/pages/userInfo/userInfo.js
+++ smallProject/pages/userInfo/userInfo.js
@@ -1,15 +1,20 @@
 Page({
   data: {
-    nickName: '',  //用户昵称
     avatarUrl: '',  //用户头像
+    userPhone: '',//用户手机号
+    coupon:'2', //优惠券
+    memberPoints:'556' //会员积分
   },
   //  // 页面初始化 options为页面跳转所带来的参数
    onLoad: function (options) {
      var userInfo = wx.getStorageSync('userInfo').userInfo;
      console.log(userInfo);
+     var userPhone = '18295176350'; //用户手机号从本地存储中获取
+     userPhone = userPhone.substr(0, 3) + '****' + userPhone.substr(7); //隐藏中间四位 
      this.setData({
        nickName: userInfo.nickName,
-       avatarUrl: userInfo.avatarUrl
+       avatarUrl: userInfo.avatarUrl,
+       userPhone: userPhone
      })
    },
   //  // 页面渲染完成

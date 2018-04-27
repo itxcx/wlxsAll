cee1b370dacde5752dd1efee2d73157bd108c4b4
saniaorongxue <277651205@qq.com>
2018-04-27 21:23:26 +0800

    update miniprogram function for update

--- smallProject/app.js
+++ smallProject/app.js
@@ -1,6 +1,35 @@
 App({
   onLaunch: function () {
-    var global_this = this;
+    const updateManager = wx.getUpdateManager();
+    console.log('updateManager')
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate);
+      console.log('ssssss')
+
+    })
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate();
+          }
+        }
+      })
+    })
+    updateManager.onUpdateFailed(function () {
+      // 新的版本下载失败
+
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本下载失败',
+        showCancel: false
+      })
+    })
+
   },
  
   globalData: {

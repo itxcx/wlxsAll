0dcaca5dda781f94bbc0f3336e8824e179adf0ee
saniaorongxue <277651205@qq.com>
2018-05-02 19:13:42 +0800

    update repertory

---
+++ backstage/supplier/src/components/Repertory.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="Repertory">
+      <header>
+
+      </header>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "repertory",
+      data() {
+        return {
+
+        }
+      },
+      methods: {
+
+      }
+    }
+</script>
+
+<style lang="less">
+  .Repertory{
+    background: #fff;
+    header{
+      background: #5fcf6c;
+      height: 7.496vh;
+    }
+  }
+</style>

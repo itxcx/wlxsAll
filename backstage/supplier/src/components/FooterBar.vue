a62c6ddbf5f214eeb80101b67886c5e54bdaa8d1
saniaorongxue <277651205@qq.com>
2018-05-02 14:46:36 +0800

    add some files

---
+++ backstage/supplier/src/components/FooterBar.vue
@@ -0,0 +1,99 @@
+<template>
+  <footer>
+    <dl>
+      <dt :class="{'platformStyle1':!platform,'platformStyle2':platform}"></dt>
+      <dd :class="{'ddStyle1':!platform,'ddStyle2':platform}">工作平台</dd>
+    </dl>
+    <dl>
+      <dt :class="{'statementStyle1':!statement,'statementStyle2':statement}"></dt>
+      <dd :class="{'ddStyle1':!statement,'ddStyle2':statement}">智能报表</dd>
+    </dl>
+    <dl>
+      <dt :class="{'personalStyle1':!personal,'personalStyle2':personal}"></dt>
+      <dd :class="{'ddStyle1':!personal,'ddStyle2':personal}">个人中心</dd>
+    </dl>
+  </footer>
+</template>
+<script>
+  export default ({
+    name: 'FooterBar',
+    data() {
+      return {
+        platform: true,
+        statement: false,
+        personal: false
+      }
+    },
+    mounted() {
+      this.$nextTick( () => {
+
+      })
+    },
+    methods: {
+
+    }
+  })
+</script>
+<style lang="less">
+  @footerHeight: 7.49625vh;
+  footer{
+    border-top: 1px solid #cccccc;
+    width: 100%;
+    position: fixed;
+    background: #fff;
+    height: @footerHeight;
+    left: 0;
+    bottom: 0;
+    text-align: center;
+    padding-top: 5px;
+    z-index: 999;
+    dl{
+      width: 33.3333%;
+      height: @footerHeight;
+      float: left;
+      text-align: center;
+      cursor: pointer;
+      dt{
+        width: 6.4vw;
+        height: 3.283vh;
+        margin: 0 auto;
+      }
+      dd{
+        margin-top: 0.5997vh;
+        font-size: 1.649rem;
+      }
+    }
+    .ddStyle1{
+      color: #a3a7aa;
+    }
+    .ddStyle2{
+      color: #65d072;
+    }
+    .platformStyle1{
+      background: url('../../static/images/platform2.png') no-repeat center center;
+      background-size: cover;
+    }
+    .statementStyle1{
+      background: url('../../static/images/report-forms.png') no-repeat center center;
+      background-size: cover;
+    }
+    .personalStyle1{
+      background: url('../../static/images/personal.png') no-repeat center center;
+      background-size: cover;
+    }
+
+    .platformStyle2{
+      background: url('../../static/images/platform1.png') no-repeat center center;
+      background-size: cover;
+    }
+    .statementStyle2{
+      background: url('../../static/images/report-forms1.png') no-repeat center center;
+      background-size: cover;
+    }
+    .personalStyle2{
+      background: url('../../static/images/personal1.png') no-repeat center center;
+      background-size: cover;
+    }
+  }
+</style>
+
---
+++ backstage/supplier/src/components/Main.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="Main">
+      <section class="topBanner"></section>
+      <section class="repertory">
+        <div></div>
+        <dl>
+          <dt>售货柜库存</dt>
+          <dd>查询售货柜库存，进行实时补货</dd>
+        </dl>
+      </section>
+      <section class="handle">
+        <aside class="exhibit">
+          <div></div>
+          <dl>
+            <dt>商品上架</dt>
+            <dd>放入售货柜中</dd>
+          </dl>
+        </aside>
+        <aside class="ship">
+          <div></div>
+          <dl>
+            <dt>商品下架</dt>
+            <dd>售货柜中取出</dd>
+          </dl>
+        </aside>
+      </section>
+      <Footer-bar></Footer-bar>
+    </div>
+</template>
+
+<script>
+  import FooterBar from './FooterBar.vue';
+    export default {
+      name: "Main",
+      data() {
+          return {}
+      },
+      components:{
+        FooterBar
+      },
+      methods: {
+
+      }
+    }
+</script>
+
+<style lang="less">
+  .Main{
+    background: #f5f5f5;
+    .topBanner{
+      width: 100vw;
+      height: 23.2383vh;
+      background: url("../../static/images/banner.jpg") no-repeat center center;
+      background-size: cover;
+    }
+    .repertory{
+      background: #ffffff;
+      margin: 2.667vw;
+      padding: 2.6236vh 4vw;
+      border-radius: 5px;
+      overflow: hidden;
+      div{
+        background: url("../../static/images/stock.png") no-repeat center center;
+        background-size: cover;
+        width: 12vw;
+        height: 12.1vw;
+        float: left;
+        margin-right: 3.333vw;
+      }
+      dl{
+        float: left;
+        padding-top: 0.6vh;
+        dt{
+          font-size: 2.248rem;
+        }
+        dd{
+          font-size: 1.949rem;
+        }
+      }
+    }
+    .handle{
+      display: flex;
+
+      .exhibit, .ship{
+        background: #fff;
+        border-radius: 5px;
+        width: 46.2666vw;
+        margin-left: 2.667vw;
+        padding: 2.6236vh 4vw;
+        div{
+          width: 12vw;
+          height: 12.1vw;
+          float: left;
+          margin-right: 3.333vw;
+        }
+        dl{
+          padding-top: 0.6vh;
+          dt{
+            font-size: 2.248rem;
+          }
+          dd{
+            font-size: 1.949rem;
+          }
+        }
+      }
+      .exhibit div{
+        background: url("../../static/images/shelf.png") no-repeat center center;
+        background-size: cover;
+      }
+      .ship div{
+        background: url("../../static/images/take.png") no-repeat center center;
+        background-size: cover;
+      }
+    }
+  }
+</style>

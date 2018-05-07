ffffd5c7e2895d0c8150586b978ef607586d7039
saniaorongxue <277651205@qq.com>
2018-05-07 09:22:20 +0800

    update index

--- backstage/supplier/src/router/index.js
+++ backstage/supplier/src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 //路由模块引入
-import Login from '../components/Login'
-import Main from '../components/Main' //首页
+import Login from '../components/Login' //登录页面
+import Main from '../components/Main' //主页面
 import Statement from '../components/Statement' //智能报表
 import Personal from '../components/Personal' //个人中心
 import Repertory from '../components/Repertory' //库存
@@ -10,8 +10,6 @@ import Exhibing from '../components/Exhibing' //上货
 import DoorClose from '../components/DoorClose' //关门
 import InStoreDone from '../components/InStoreDone' //入库完成
 //引入主要的数据模块
-
-
 
 Vue.use(Router);
 

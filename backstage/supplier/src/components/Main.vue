475b7b34252fe3c383478770497954f71aded0e3
saniaorongxue <277651205@qq.com>
2018-05-13 15:19:32 +0800

    update main component

--- backstage/supplier/src/components/Main.vue
+++ backstage/supplier/src/components/Main.vue
@@ -41,6 +41,7 @@
       //页面初始化
       mounted() {
         this.$nextTick(() => {
+          //发送请求，获取配置参数
           this.getConfigParames();
         })
       },
@@ -54,12 +55,12 @@
         //获取config
         getConfigParames() {
           wx.config({
-            debug : true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId : _appId, // 必填，公众号的唯一标识
-            timestamp : _timestamp, // 必填，生成签名的时间戳
-            nonceStr : _nonceStr, // 必填，生成签名的随机串
-            signature : _signature, // 必填，签名，见附录1
-            jsApiList : ['scanQRCode'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            debug : true,
+            appId : _appId,
+            timestamp : _timestamp,
+            nonceStr : _nonceStr,
+            signature : _signature,
+            jsApiList : ['scanQRCode']
           });
         },
         //上货
